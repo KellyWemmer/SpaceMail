1a98acea2e3694190ea8a9592665967b4ac52b18
Kelly <clear>
2022-08-13 11:37:05 -0600

    server controller

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
+import { SpaceShipSchema } from '../models/SpaceShip';
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+  SpaceShips = mongoose.model('SpaceShip', SpaceShipSchema)
 }
 
 export const dbContext = new DbContext()
